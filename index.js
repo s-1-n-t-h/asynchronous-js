d73d7269296fd7477e71bdbe00d09366db52d021
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-23 21:16:55 +0530

    nested callbacks

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 console.log("before");
-getUser(1,(user) => console.log(user));
+getUser(1,(user) => {
+    console.log(user)
+    getRepositories(user.gitHubUserName, (repos) => console.log("Repos: ",repos))
+});
 console.log("after");
 
 function getUser(id, callback) {
 	setTimeout(() => {
-        console.log("Reading user from DB...")
+        console.log("\nReading user from DB...")
 		callback({ id, gitHubUserName: "yash" });
 	}, 2000);
+}
+
+function getRepositories(username, callback){
+    setTimeout(() => {
+        console.log(`\nCalling GitHub API`);
+        callback(['repo1', 'repo2', 'repo3'])
+    })
 }
