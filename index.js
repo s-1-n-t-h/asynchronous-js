b9a17037d7b05f27c92778d7f531bb658c731a2c
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-23 22:50:45 +0530

    converting hell into promise objects

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 console.log("before");
-getUser(1,(user) => {
-    console.log(user)
-    getRepositories(user.gitHubUserName, (repos) => console.log("Repos: ",repos))
-});
+getUser(
+    1,
+    (user) => {
+        console.log(user);
+        getRepositories(
+            user.gitHubUserName,
+            (repos) => {
+                console.log("Repos: ", repos);
+                getCommits(
+                    repos[0],
+                    (commits) => {
+                        console.log("\ncommits: ", commits);
+                    },
+                );
+            },
+        );
+    },
+);
 console.log("after");
 
-function getUser(id, callback) {
-	setTimeout(() => {
-        console.log("\nReading user from DB...")
-		callback({ id, gitHubUserName: "yash" });
-	}, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            () => {
+                console.log("\nReading user from DB...");
+                resolve({ id, gitHubUserName: "yash" });
+            },
+            2000,
+        );
+    },);
 }
 
-function getRepositories(username, callback){
-    setTimeout(() => {
-        console.log(`\nCalling GitHub API`);
-        callback(['repo1', 'repo2', 'repo3'])
-    })
+function getRepositories(username) {
+    new Promise((resolve, reject) => {
+        setTimeout(
+            () => {
+                console.log(`\nCalling GitHub API`);
+                resolve(["repo1", "repo2", "repo3"]);
+            },
+            2000,
+        );
+    },); 
+}
+
+function getCommits(commits) {
+    new Promise((resolve, reject) => {
+        setTimeout(
+            () => {
+                console.log(`\nCalling GitHub API`);
+                resolve(commits);
+            },
+            2000,
+        );
+    },);
 }
