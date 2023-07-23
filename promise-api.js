c5832f1830fc6c8a40dc39a5c177289986593679
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-23 23:33:41 +0530

    running parallel promises and race them

--- promise-api.js
+++ promise-api.js
@@ -1,13 +1,13 @@
 const p1 = new Promise((resolve, reject) => {
     console.log("Asynchronous call 1...")
-    setTimeout(() => reject(new Error("something went wrong")), 2000)
+    setTimeout(() => resolve(1), 2000)
 })
 
 const p2 = new Promise((resolve) => {
     console.log("Asynchronous call 2...");
-    setTimeout(() => resolve("call 2 finished"), 2000);
+    setTimeout(() => resolve(2), 2000);
 },);
 
-Promise.all([p1,p2])
+Promise.race([p1,p2])
     .then(result => console.log(result))
-    .catch(err => console.log(err.message))
+    .catch(err => console.log(err.message)) 
