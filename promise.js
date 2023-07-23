75bd73bb1df77d9c62317357f56630fbe5c5b72b
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-23 22:06:55 +0530

    promises and handling them

---
+++ promise.js
@@ -0,0 +1,9 @@
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => 
+    //resolve(1), // pending => fulfilled / resolved
+    reject(new Error("Some Error Occured")), 2000) // pending => rejected 
+})
+
+p
+    .then(response => console.log("Result",response))
+    .catch(err => console.log(err))
