7041bbd36fffff5e5a31a3ebe20dde9b0b5b1c01
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-23 23:30:51 +0530

    running parallel promises - case of rejection

--- promise-api.js
+++ promise-api.js
@@ -1,6 +1,6 @@
-const p1 = new Promise((resolve) => {
+const p1 = new Promise((resolve, reject) => {
     console.log("Asynchronous call 1...")
-    setTimeout(() => resolve("call 1 finished"), 2000)
+    setTimeout(() => reject(new Error("something went wrong")), 2000)
 })
 
 const p2 = new Promise((resolve) => {
@@ -10,3 +10,4 @@ const p2 = new Promise((resolve) => {
 
 Promise.all([p1,p2])
     .then(result => console.log(result))
+    .catch(err => console.log(err.message))
